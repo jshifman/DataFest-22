d5a64a17110edb08e9ffc4c9a3ef9081bcf9f4ef
jshifman <josephshifman@gmail.com>
2022-04-02 14:45:37 -0700

    plotss

--- Run-Log.R
+++ Run-Log.R
@@ -1,13 +1,13 @@
 library(tidyverse)
-log.files <- read_csv("data/DataFest2022/data files/logs.csv", guess_max = 2106600)
+log.files <- read_csv("data/DataFest2022/data files/logs.csv", guess_max = 2106600, col_select = c(date, total_points, event_id, player_id))
 
 people_data <- log.files
 people_data$date <- as.Date.character(people_data$date)
-
+people_data <- filter(people_data, date > "1975-01-01")
 
 people_data <- people_data %>%
   group_by(player_id) %>%
-  mutate(initial_date = min(.$date),time_since_initial = date - initial_date)
+  mutate(initial_date = min(date),time_since_initial = as.numeric(date - initial_date)/7)
   
 total_bad <- people_data %>%
   group_by(minigame_level, player_id) %>%
@@ -26,4 +26,26 @@ three_strikes <- drop_na(three_strikes)
 write.csv(avg_t_bad, file = "bad_choices.csv")
 write.csv(three_strikes, file = "three_strikes.csv")
 
-ggplot(people_data,aes(x=minigame_level,))
+avg_t_bad <- read.csv("bad_choices.csv")
+three_strikes <- read.csv("three_strikes.csv")
+
+three_strikes$minigame_level <- as.factor(three_strikes$minigame_level)
+avg_t_bad$minigame_level <- as.factor(avg_t_bad$minigame_level)
+
+library(ggthemes)
+ggplot() +
+  theme_hc() +
+  geom_col(data = three_strikes, aes(x = minigame_level, y = total), fill = "blue", alpha = .3) +
+  geom_line(data = avg_t_bad, aes(x = minigame_level, y = avg_bad*8, group = 1)) +
+  geom_point(data = avg_t_bad, aes(x = minigame_level, y = avg_bad*8, group = 1)) +
+  scale_y_continuous("Total Minigame Losses (Bar)", sec.axis = sec_axis(~./8, name = "Average Total Bad Decisions Made (Line)")) +
+  xlab("Minigame Level") +
+  ggtitle("Aggregate Totals from Friends Minigame")
+
+
+people_data <- filter(people_data, event_id == 911)
+
+ggplot() +
+  geom_point(data = people_data, aes(x = time_since_initial, y = total_points)) +
+  geom_smooth(data = people_data, aes(x = time_since_initial, y = total_points)) +
+  ylim(c(0, 50))
